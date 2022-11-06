611eb1bb78b79be869e7f0591b3d6193400ab4e1
doctafaustus <clowd150@gmail.com>
2022-11-05 19:37:44 -0500

    Add dynamic port

--- app.js
+++ app.js
@@ -10,6 +10,6 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/index.html'));
 });
 
-app.listen(port, () => {
+app.listen(process.env.PORT || port, () => {
   console.log(`App listening on port ${port}`)
 });
